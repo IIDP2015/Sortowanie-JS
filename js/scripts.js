0fa673c257b033fa4831bb226e8db3c4c2713093
Szczepan Zaskalski <szczepix@goldrapid.pl>
2018-01-24 18:57:20 +0100

    Finished project

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,21 @@
 (function() {
-  let table = document.querySelector("#myTable"),
+  var table = document.querySelector("#myTable"),
     ths = table.querySelectorAll("thead th"),
     trs = table.querySelectorAll("tbody tr");
-
   function makeArray(nodeList) {
-    let arr = [];
-    for (let i = 0; i < arr.length; i++) {
-      const element = arr[i];
+    var arr = [];
+    for (var i = 0; i < nodeList.length; i++) {
+      arr.push(nodeList[i]);
     }
     return arr;
   }
-
   function clearClassName(nodeList) {
-    for (let i = 0; i < nodeList.length; i++) {
+    for (var i = 0; i < nodeList.length; i++) {
       nodeList[i].className = "";
     }
   }
-
-  function sortBy(el) {
-    let target = el.target,
+  function sortBy(e) {
+    var target = e.target,
       thsArr = makeArray(ths),
       trsArr = makeArray(trs),
       index = thsArr.indexOf(target),
@@ -26,9 +23,8 @@
       order =
         target.className === "" || target.className === "desc" ? "asc" : "desc";
     clearClassName(ths);
-
     trsArr.sort(function(a, b) {
-      let tdA = a.children[index].textContent,
+      var tdA = a.children[index].textContent,
         tdB = b.children[index].textContent;
       if (tdA < tdB) {
         return order === "asc" ? -1 : 1;
@@ -38,16 +34,13 @@
         return 0;
       }
     });
-
     trsArr.forEach(function(tr) {
       df.appendChild(tr);
     });
-
     target.className = order;
     table.querySelector("tbody").appendChild(df);
   }
-
-  for (let i = 0; i < ths.length; i++) {
-    ths[i].onClick = sortBy;
+  for (var i = 0; i < ths.length; i++) {
+    ths[i].onclick = sortBy;
   }
 })();
