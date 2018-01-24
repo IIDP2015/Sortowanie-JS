145cbbb78faaf3bd698911cac9c3f9cce2a6077e
Szczepan Zaskalski <szczepix@goldrapid.pl>
2018-01-24 18:43:37 +0100

    rewritten code from comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 (function() {
-  var table = document.querySelector("#myTable"),
+  let table = document.querySelector("#myTable"),
     ths = table.querySelectorAll("thead th"),
     trs = table.querySelectorAll("tbody tr");
 
@@ -11,11 +11,25 @@
   //     return arr;
   //   }
 
+  function makeArray(nodeList) {
+    let arr = [];
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+    }
+    return arr;
+  }
+
   //   function clearClassName(nodeList) {
   //     for (var i = 0; i < nodeList.length; i++) {
   //       nodeList[i].className = "";
   //     }
   //   }
+
+  function clearClassName(nodeList) {
+    for (let i = 0; i < nodeList.length; i++) {
+      nodeList[i].className = "";
+    }
+  }
 
   //   function sortBy(e) {
   //     var target = e.target,
@@ -44,7 +58,41 @@
   //     table.querySelector("tbody").appendChild(df);
   //   }
 
+  function sortBy(el) {
+    let target = el.target,
+      thsArr = makeArray(ths),
+      trsArr = makeArray(trs),
+      index = thsArr.indexOf(target),
+      df = document.createDocumentFragment(),
+      order =
+        target.className === "" || target.className === "desc" ? "asc" : "desc";
+    clearClassName(ths);
+
+    trsArr.sort(function(a, b) {
+      let tdA = a.children[index].textContent,
+        tdB = b.children[index].textContent;
+      if (tdA < tdB) {
+        return order === "asc" ? -1 : 1;
+      } else if (tdA > tdB) {
+        return order === "asc" ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+
+    trsArr.forEach(function(tr) {
+      df.appendChild(tr);
+    });
+
+    target.className = order;
+    table.querySelector("tbody").appendChild(df);
+  }
+
   //   for (var i = 0; i < ths.length; i++) {
   //     ths[i].onclick = sortBy;
   //   }
+
+  for (let i = 0; i < ths.length; i++) {
+    ths[i].onClick = sortBy;
+  }
 })();
